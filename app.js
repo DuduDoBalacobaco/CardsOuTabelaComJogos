69aeca99316a025c4e4bca20b6887f3179ff9880
Carlos Eduardo <ydudu1009y@gmail.com>
2026-06-21 05:47:35 -0300

    Melhorias em geral

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 const express = require('express');
 const app = express();
 
+let jogos = [
+  { id: 1, nome: "Minecraft", genero: "Sandbox", ano: 2011 },
+  { id: 2, nome: "Elden Ring", genero: "RPG", ano: 2022 },
+  { id: 3, nome: "RDR 2", genero: "Ação", ano: 2018 },
+  { id: 4, nome: "Fortnite", genero: "Battle Royale", ano: 2017 },
+  { id: 5, nome: "Tetris", genero: "Puzzle", ano: 1988},
+  { id: 6, nome: "Super Mario Bros.", genero: "Aventura", ano: 1985}
+];
+let listaDeJogos = jogos.length;
+
+app.use(express.json());
 
 app.get("/jogos", (req, res) => {
-  const jogos = [
-      { id: 1, nome: "Minecraft", genero: "Sandbox", ano: 2011 },
-      { id: 2, nome: "Elden Ring", genero: "RPG", ano: 2022 },
-      { id: 3, nome: "RDR 2", genero: "Ação", ano: 2018 },
-      { id: 4, nome: "Fortnite", genero: "Battle Royale", ano: 2017 },
-      { id: 5, nome: "Tetris", genero: "Puzzle", ano: 1988},
-      { id: 5, nome: "Super Mario Bros.", genero: "Aventura", ano: 1985}
-   ];
   res.json(jogos);
 });
 
-app.use(
-    express.static("exerciciojogos/public")
-);
+app.post("/jogos", (req, res) => {
+  listaDeJogos = listaDeJogos + 1;
+
+  const jogo = {
+    id: listaDeJogos,
+    nome: req.body.nome,
+    genero: req.body.genero,
+    ano: req.body.ano
+  };
+
+  jogos.push(jogo);
+
+  res.json(jogo);
+});
+
+app.put("/jogos/:id", (req, res) => {
+  const jogo = jogos.find(
+      u => u.id == req.params.id
+  );
+
+  jogo.nome = req.body.nome === "" || req.body.nome == null ? jogo.nome : req.body.nome;
+  jogo.genero = req.body.genero === "" || req.body.genero == null ? jogo.genero : req.body.genero;
+  jogo.ano = req.body.ano === "" || req.body.ano == null ? jogo.ano : req.body.ano;
+
+  res.json(jogo);
+});
+
+app.delete("/jogos/:id", (req, res) => {
+  let tamanho = jogos.length
+
+  jogos = jogos.filter(
+      u => u.id != req.params.id
+  );
+
+  jogos.length === tamanho ? res.send("Jogo não encontrado") : res.send("Jogo removido")
+});
+
+app.use(express.static("public"));
 
 app.listen(3000);
--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
+let cadastrarNome = document.getElementById('cadastrarNomeJogo')
+let cadastrarGenero = document.getElementById('cadastrarGeneroJogo')
+let cadastrarAno = document.getElementById('cadastrarAnoJogo')
+
+let alterarNome = document.getElementById('alterarNomeJogo')
+let alterarGenero = document.getElementById('alterarGeneroJogo')
+let alterarAno = document.getElementById('alterarAnoJogo')
+
+let idDeletar = document.getElementById('deletarJogo')
+
 async function carregaJogos() {
-   const resposta = await fetch("/jogos");
-   const jogos = await resposta.json();
+    const resposta = await fetch("/jogos");
+    const jogos = await resposta.json();
 
     let opcao = document.getElementById('switch');
 
@@ -10,6 +20,137 @@ async function carregaJogos() {
     else{
         tabela(jogos);
     }
+} 
+
+async function verificarExistenciaDeID(id, frase, cor){
+    const resposta = await fetch("/jogos");
+    const jogos = await resposta.json();
+
+    let idVerificar = Number(document.getElementById(id).value);
+
+    let existe = jogos.some(u => u.id === idVerificar)
+
+    if(existe && id === 'deletarJogo'){
+        mudarDisplayExcluir()
+    }
+    else if(existe && id === 'alterarJogo'){
+        abrirAlterar()
+    }
+    else{
+        let p = document.getElementById(frase)
+        p.textContent = "ID não existe"
+        p.style.color = "red"
+        document.getElementById(cor).style.display = "none"
+
+        setTimeout(() =>{
+            p.textContent = "Digite o id do jogo que quer"
+            p.style.color = "rgb(223, 221, 221)"
+            document.getElementById(cor).style.display = "flex"
+        }, 3000)
+        limparInput(document.getElementById(id))
+    }
+}
+
+function limparInput(input){
+    input.value = "";
+}
+
+async function cadastrarJogo(nomeJogo, generoJogo, anoJogo) {
+    await fetch("/jogos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nome: nomeJogo, genero: generoJogo, ano: anoJogo})
+    });
+
+    limparInput(cadastrarNome)
+    limparInput(cadastrarGenero)
+    limparInput(cadastrarAno)
+
+    carregaJogos()
+}
+
+function capturarTexto(){
+    nome = cadastrarNome.value;
+    nome = nome === "" ? "Não definido" : formatarTexto(nome)
+
+    genero = cadastrarGenero.value;
+    genero = genero === "" ? "Não definido" : formatarTexto(genero)
+
+    ano = cadastrarAno.value;
+    ano = ano === "" ? "Não definido" : ano
+
+    cadastrarJogo(nome, genero, ano)
+}
+
+async function alterarJogo(id, nomeJogo, generoJogo, anoJogo){
+    await fetch(`/jogos/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nome: nomeJogo, genero: generoJogo, ano: anoJogo})
+    });
+    
+    alterarDisplayAlterar()
+    carregaJogos()
+}
+
+function capturarId(){
+    let id = document.getElementById('alterarJogo').value
+    limparInput(document.getElementById('alterarJogo'))
+    capturarAlteracao(id)
+}
+function capturarAlteracao(id){
+    let nome = alterarNome.value
+    nome = formatarTexto(nome)
+    limparInput(alterarNome)
+
+    let genero = alterarGenero.value
+    genero = formatarTexto(genero)
+    limparInput(alterarGenero)
+
+    let ano = alterarAno.value
+    limparInput(alterarAno)
+
+    alterarJogo(id, nome, genero, ano)
+}
+
+async function excluirJogo(id) {
+   await fetch(`/jogos/${id}`, {
+       method: "DELETE"
+   });
+
+   limparInput(idDeletar)
+   alterarDisplayDeletar()
+   carregaJogos()
+}
+
+function mudarDisplayExcluir(){
+    deletacao.style.display = "none";
+    confirmarExcluir.style.display = "flex";
+}
+
+function confirmarExclusaoJogo(){
+    let id = idDeletar.value;
+    excluirJogo(id)
+}
+
+function retornarDisplayExcluir(){
+    confirmarExcluir.style.display = "none";
+    deletacao.style.display = "flex";
+    limparInput(idDeletar)
+}
+
+function formatarTexto(text){
+    let texto = text.trim().toLowerCase();
+    let formatado = "";
+
+    for(let i = 0; i < texto.length; i++){
+        formatado += (i === 0) || (texto.charAt(i - 1) === " ") ? texto.charAt(i).toUpperCase() : texto.charAt(i)
+    }
+    return formatado
 }
 
 function card(games){
@@ -52,64 +193,117 @@ function card(games){
 
 function tabela(games){
 
-    let jogos = games;
+    let mostrar = document.getElementById('mostrar');
+    mostrar.textContent = "";
+    
+    let tabela = document.createElement('table');
+    tabela.id = 'tabela';
+    tabela.border = '1';
 
-        let mostrar = document.getElementById('mostrar');
-        mostrar.textContent = "";
-        
-        let tabela = document.createElement('table');
-        tabela.id = 'tabela';
-        tabela.border = '1';
+    let tr1 = document.createElement('tr');
 
-        let tr1 = document.createElement('tr');
+    let thId = document.createElement('th');
+    thId.textContent = 'ID';
 
-        let thId = document.createElement('th');
-        thId.textContent = 'ID';
+    let thNome = document.createElement('th');
+    thNome.textContent = 'Nome';
 
-        let thNome = document.createElement('th');
-        thNome.textContent = 'Nome';
+    let thGenero = document.createElement('th');
+    thGenero.textContent = 'Gênero';
 
-        let thGenero = document.createElement('th');
-        thGenero.textContent = 'Gênero';
+    let thAno = document.createElement('th');
+    thAno.textContent = 'Lançamento';
 
-        let thAno = document.createElement('th');
-        thAno.textContent = 'Lançamento';
+    tr1.appendChild(thId);
+    tr1.appendChild(thNome);
+    tr1.appendChild(thGenero);
+    tr1.appendChild(thAno);
 
-        tr1.appendChild(thId);
-        tr1.appendChild(thNome);
-        tr1.appendChild(thGenero);
-        tr1.appendChild(thAno);
+    tabela.appendChild(tr1);
 
-        tabela.appendChild(tr1);
+    games.forEach(jogo => {
 
-        games.forEach(jogo => {
+        let tr = document.createElement('tr');
 
-            let tr = document.createElement('tr');
+        let tdId = document.createElement('td');
+        tdId.textContent = jogo.id;
 
-            let tdId = document.createElement('td');
-            tdId.textContent = jogo.id;
+        let tdNome = document.createElement('td');
+        tdNome.textContent = jogo.nome;
 
-            let tdNome = document.createElement('td');
-            tdNome.textContent = jogo.nome;
+        let tdGenero = document.createElement('td');
+        tdGenero.textContent = jogo.genero;
 
-            let tdGenero = document.createElement('td');
-            tdGenero.textContent = jogo.genero;
+        let tdAno = document.createElement('td');
+        tdAno.textContent = jogo.ano;
 
-            let tdAno = document.createElement('td');
-            tdAno.textContent = jogo.ano;
+        tr.appendChild(tdId);
+        tr.appendChild(tdNome);
+        tr.appendChild(tdGenero);
+        tr.appendChild(tdAno);
 
-            tr.appendChild(tdId);
-            tr.appendChild(tdNome);
-            tr.appendChild(tdGenero);
-            tr.appendChild(tdAno);
+        tabela.appendChild(tr);
+    });
 
-            tabela.appendChild(tr);
-        });
-
-        mostrar.appendChild(tabela);
+    mostrar.appendChild(tabela);
 }
 
 function mudarLayout(){
     carregaJogos();
 }
+
+let alteracao = document.getElementsByClassName('alteracao')[0];
+let deletacao = document.getElementsByClassName('deletar')[0];
+let cadastracao = document.getElementsByClassName('cadastro')[0];
+
+let cadastroJogo = document.getElementById('cadastrar');
+let mudarJogo = document.getElementById('alterar');
+let excluirGame = document.getElementById('excluir');
+
+let idAlterar = document.getElementById('idAlterar');
+let alterarCaracteristica = document.getElementById('alterarCaracteristica');
+let confirmarExcluir = document.getElementById('confirmarExcluir');
+
+function alterarDisplayCadastro(){
+    alteracao.style.display = "none";
+    deletacao.style.display = "none";
+    cadastracao.style.display = "flex"
+    confirmarExcluir.style.display = "none";
+
+    mudarJogo.style.border = "1px solid #f0f0f0";
+    excluirGame.style.border = "1px solid #f0f0f0";
+    cadastroJogo.style.borderBottom = "none";
+}
+
+function alterarDisplayAlterar(){
+    cadastracao.style.display = "none";
+    deletacao.style.display = "none";
+    alteracao.style.display = "flex";
+    alterarCaracteristica.style.display = "none";
+    confirmarExcluir.style.display = "none";
+
+    mudarJogo.style.borderBottom = "none";
+    excluirGame.style.border = "1px solid #f0f0f0";
+    cadastroJogo.style.border = "1px solid #f0f0f0";
+    idAlterar.style.display = "flex"
+}
+
+function alterarDisplayDeletar(){
+    cadastracao.style.display = "none";
+    deletacao.style.display = "flex";
+    alteracao.style.display = "none";
+    confirmarExcluir.style.display = "none";
+
+    mudarJogo.style.border = "1px solid #f0f0f0";
+    excluirGame.style.borderBottom = "none";
+    cadastroJogo.style.border = "1px solid #f0f0f0";
+}
+
+function abrirAlterar(){
+    idAlterar.style.display = "none"
+    alterarCaracteristica.style.display = "flex";
+}
+
+alterarDisplayCadastro()
+
 carregaJogos();
